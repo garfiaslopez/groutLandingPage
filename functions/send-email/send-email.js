ed3976fdf9528d8e02fb24935f8f61be055e3f15
Jose Garfias Lopez <garfiaslopez@hotmail.com>
2020-05-03 15:49:25 -0500

    update send email lambda function

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -2,33 +2,31 @@ const SparkPost = require('sparkpost');
 const client = new SparkPost(process.env.SPARKPOST);
 
 exports.handler = async (event, context) => {
-  try {
-    const name = event.queryStringParameters.name || 'No Name'
-    const email = event.queryStringParameters.name || 'No email'
-    const phone = event.queryStringParameters.name || 'No phone'
-    const message = event.queryStringParameters.name || 'No Message'
+  const name = event.queryStringParameters.name || 'No Name'
+  const email = event.queryStringParameters.email || 'No email'
+  const phone = event.queryStringParameters.phone || 'No phone'
+  const message = event.queryStringParameters.message || 'No Message'
 
-    const htmlBody = '';
-    [['name',name],['email',email],['phone',phone],['message',message]].forEach((e) => {
-      htmlBody += '<p>' + e[0] + ': ' + e[1] + '</p><br>'
-    });
-    client.transmissions.send({
+  let htmlBody = '';
+  [['name',name],['email',email],['phone',phone],['message',message]].forEach((e) => {
+    htmlBody += '<p>' + e[0] + ': ' + e[1] + '</p><br>'
+  });
+
+  try {
+    await client.transmissions.send({
       content: {
           from: 'contact@email.pciflooringcanada.com',
           subject: 'Contact from pciflooringcanada',
           html:
             '<html><body>' + htmlBody + '</body></html>'
         },
-        recipients: [{ address: 'pciflooringcontractor@gmail.com' }, { address: 'garfiaslopez@hotmail.com' }]
-    }).then(() => {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ success: true })
-      }
-    }).catch((err)=>{
-      return { statusCode: 500, body: err.toString() }
+        recipients: [ { address: 'pciflooringcontractor@gmail.com' }, { address: 'garfiaslopez@hotmail.com' }]
     });
-  } catch (err) {
+  } catch(err) {
     return { statusCode: 500, body: err.toString() }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success: true })
   }
 }
